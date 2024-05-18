da40fc0ee94d3592b79d40da5d0cfd8488fa0726
Jeremiah Wafula <jeremiahwafula01@gmail.com>
2024-05-18 14:04:40 +0300

    initial commit

---
+++ src/main/java/com/tms/Task/Management/System/API/TaskManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.tms.Task.Management.System.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tms/Task/Management/System/API/TaskManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.Task.Management.System.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
